ef6e597ee743ebdd3927f7045574dee3010122e4
rohanBrid18 <bridrohan1122@gmail.com>
2020-10-29 23:01:24 +0530

    encoded data using huffman tree

--- huffman.py
+++ huffman.py
@@ -54,3 +54,33 @@ count = 0
 for level in huff:
     print("Level", count, ":", level)
     count += 1
+print()
+
+def encode(characters, checklist, data):
+    char_bin = []
+    if len(characters) == 1:
+        char_code = [characters[0], "0"]
+        char_bin.append(char_code * len(data))
+    else:
+        for char in characters:
+            charcode = ""
+            for node in checklist:
+                if len(node) > 2 and char in node[1]:
+                    charcode += node[2]
+            char_code = [char, charcode]
+            char_bin.append(char_code)
+
+    for item in char_bin:
+        print(item[0], item[1])
+
+    bitstring = ""
+    for char in data:
+        for item in char_bin:
+            if char in item:
+                bitstring += item[1]
+    return bitstring
+
+encoded_data = encode(characters, checklist, data)
+
+print("\nEncoded Data: {}\n".format(encoded_data))
+print("Compressed data size: {} bits\n".format(len(encoded_data)))
